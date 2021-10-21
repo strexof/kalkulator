792f222d2aeae911629fd64b4abd7b260d0948eb
Strexof <92519593+strexof@users.noreply.github.com>
2021-10-21 11:23:34 +0700

    Add files via upload

---
+++ kakulator.py
@@ -0,0 +1,4 @@
+tambah1 = 10
+tambah2 = 19
+hasil = tambah1+tambah2
+print("10 + 10 = ",hasil)
---
+++ kakultor2.py
@@ -0,0 +1,4 @@
+tambah1 = int(input("ketikan angka ke 1 : "))
+tambah2 = int(input("ketikan angka ke 2 : "))
+hasil = tambah1+tambah2
+print(tambah1," + ",tambah2," = ",tambah1+tambah2)
